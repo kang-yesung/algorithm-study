4d8bb932f2d79f3dcd0325fdd163224d9f9f63c0
kang-yesung <rkddptjd5228@gmail.com>
2025-05-23 17:46:55 +0900

    알고리즘 문제 풀이

---
+++ 알고리즘 강의/5week/src/example/신고결과받기.java
@@ -0,0 +1,17 @@
+package example;
+
+import java.util.*;
+
+public class 게임맵최단거리 {
+    public static void main(String[] args) {
+
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+
+        Set<String> set = new HashSet<>(Arrays.toString());
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; )
+
+    }
+}
